6a4884b892141d5b36e3ccabae7e6a34d7ccf612
Tushar Medhi <tusharmedhi@outlook.com>
2025-11-26 14:23:21 -0400

    Built a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,32 @@
-import { Button, ButtonGroup, HStack } from "@chakra-ui/react";
+import { Grid, GridItem } from "@chakra-ui/react";
 
 function App() {
   return (
     <>
-      <Button colorPalette={"blue"}>Button</Button>
-      {/* <HStack> */}
-      <Button colorPalette={"green"}>Click me</Button>
-      <Button colorPalette={"purple"}>Click me</Button>
-      {/* </HStack> */}
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`, // mobile: nav on top, main full width
+          md: `"nav nav" "aside main"`, // desktop: nav on top, sidebar + main
+        }}
+        gridTemplateRows={{ base: "60px 1fr", md: "60px 1fr" }}
+        gridTemplateColumns={{ base: "1fr", md: "200px 1fr" }} // mobile: single column
+        gap="4"
+        h="100vh"
+      >
+        <GridItem area={"nav"} bg="blue.300">
+          Navigation
+        </GridItem>
+        <GridItem
+          area={"aside"}
+          bg="green.300"
+          display={{ base: "none", lg: "block" }} // hide on small screens
+        >
+          Sidebar
+        </GridItem>
+        <GridItem area={"main"} bg="orange.300">
+          Main Content
+        </GridItem>
+      </Grid>
     </>
   );
 }
