5336add7d8c055bf3589b5a0d2b750412e7d5ae1
Tushar Medhi <tusharmedhi@outlook.com>
2025-12-12 15:50:57 -0400

    Changed Display dimensions

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,10 @@ function App() {
           base: `"nav" "main"`, // mobile: nav on top, main full width
           md: `"nav nav" "aside main"`, // desktop: nav on top, sidebar + main
         }}
+        templateColumns={{
+          base: "1fr",
+          md: "200px 1fr",
+        }}
         gridTemplateRows={{ base: "60px 1fr", md: "60px 1fr" }}
         gridTemplateColumns={{ base: "1fr", md: "200px 1fr" }} // mobile: single column
         gap="0"
@@ -23,6 +27,7 @@ function App() {
         <GridItem
           area={"aside"}
           display={{ base: "none", lg: "block" }} // hide on small screens
+          paddingX={3}
         >
           <GenreList />
         </GridItem>
--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-//import { ChakraProvider } from "@chakra-ui/react";
 import App from "./App";
 import "./index.css";
 // import theme from "./assets/theme";
 import { Provider } from "@/components/ui/provider";
 //import { defaultSystem } from "@chakra-ui/react";
-import {
-  ColorModeButton,
-  DarkMode,
-  LightMode,
-  useColorMode,
-  useColorModeValue,
-} from "@/components/ui/color-mode";
-
-import { ColorModeIcon } from "@/components/ui/color-mode";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
