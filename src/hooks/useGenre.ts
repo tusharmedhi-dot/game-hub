a54bf5d50b095a593ea9bab06f7c83b8ae0f7bf3
Tushar Medhi <tusharmedhi@outlook.com>
2025-12-19 13:41:29 -0400

    Ship to client platform data

--- src/hooks/useGenre.ts
+++ src/hooks/useGenre.ts
@@ -1,4 +1,5 @@
-import useData from "./useData";
+//import useData from "./useData";
+import genre from "@/data/genre";
 
 export interface Genre {
   id: number;
@@ -6,6 +7,7 @@ export interface Genre {
   image_background: string;
 }
 
-const useGenre = () => useData<Genre>("/genres");
+// const useGenre = () => useData<Genre>("/genres");
+const useGenre = () => ({ data: genre, isLoading: false, error: null });
 
 export default useGenre;
